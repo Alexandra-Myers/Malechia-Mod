0d89caf81b324139d25199e2fb1bfdb9e10adb46
Alexandra-Myers <bia416cat@gmail.com>
2022-03-17 15:34:56 -0400

    added item groups

--- src/main/java/net/alexandra/malechia/block/ModBlocks.java
+++ src/main/java/net/alexandra/malechia/block/ModBlocks.java
@@ -1,6 +1,7 @@
 package net.alexandra.malechia.block;
 
 import net.alexandra.malechia.MalechiaMod;
+import net.alexandra.malechia.item.ModItemGroups;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
 import net.minecraft.block.Block;
@@ -15,68 +16,68 @@ public class ModBlocks {
 
     public static final Block QUARITE_ORE = registerBlock("quarite_ore",
             new OreBlock(FabricBlockSettings.of(Material.STONE).strength(20f).requiresTool(),
-                    UniformIntProvider.create(2, 6)), ItemGroup.BUILDING_BLOCKS);
+                    UniformIntProvider.create(2, 6)), ModItemGroups.MALECHIA_BLOCKS);
     public static final Block DEEPSLATE_QUARITE_ORE = registerBlock("deepslate_quarite_ore",
             new OreBlock(FabricBlockSettings.of(Material.STONE).strength(30f).requiresTool(),
-                    UniformIntProvider.create(2, 6)), ItemGroup.BUILDING_BLOCKS);
+                    UniformIntProvider.create(2, 6)), ModItemGroups.MALECHIA_BLOCKS);
     public static final Block MYCRONIUM_ORE = registerBlock("mycronium_ore",
             new OreBlock(FabricBlockSettings.of(Material.STONE).strength(20f).requiresTool(),
-                    UniformIntProvider.create(2, 6)), ItemGroup.BUILDING_BLOCKS);
+                    UniformIntProvider.create(2, 6)), ModItemGroups.MALECHIA_BLOCKS);
     public static final Block DEEPSLATE_MYCRONIUM_ORE = registerBlock("deepslate_mycronium_ore",
             new OreBlock(FabricBlockSettings.of(Material.STONE).strength(30f).requiresTool(),
-                    UniformIntProvider.create(2, 6)), ItemGroup.BUILDING_BLOCKS);
+                    UniformIntProvider.create(2, 6)), ModItemGroups.MALECHIA_BLOCKS);
     public static final Block AURAUM_ORE = registerBlock("auraum_ore",
             new OreBlock(FabricBlockSettings.of(Material.STONE).strength(20f).requiresTool(),
-                    UniformIntProvider.create(2, 6)), ItemGroup.BUILDING_BLOCKS);
+                    UniformIntProvider.create(2, 6)), ModItemGroups.MALECHIA_BLOCKS);
     public static final Block DEEPSLATE_AURAUM_ORE = registerBlock("deepslate_auraum_ore",
             new OreBlock(FabricBlockSettings.of(Material.STONE).strength(30f).requiresTool(),
-                    UniformIntProvider.create(2, 6)), ItemGroup.BUILDING_BLOCKS);
+                    UniformIntProvider.create(2, 6)), ModItemGroups.MALECHIA_BLOCKS);
     public static final Block LOCRONITE_ORE = registerBlock("locronite_ore",
             new OreBlock(FabricBlockSettings.of(Material.STONE).strength(20f).requiresTool(),
-                    UniformIntProvider.create(2, 6)), ItemGroup.BUILDING_BLOCKS);
+                    UniformIntProvider.create(2, 6)), ModItemGroups.MALECHIA_BLOCKS);
     public static final Block DEEPSLATE_LOCRONITE_ORE = registerBlock("deepslate_locronite_ore",
             new OreBlock(FabricBlockSettings.of(Material.STONE).strength(30f).requiresTool(),
-                    UniformIntProvider.create(2, 6)), ItemGroup.BUILDING_BLOCKS);
+                    UniformIntProvider.create(2, 6)), ModItemGroups.MALECHIA_BLOCKS);
     public static final Block ALOTIC_ORE = registerBlock("alotic_ore",
             new OreBlock(FabricBlockSettings.of(Material.STONE).strength(20f).requiresTool(),
-                    UniformIntProvider.create(2, 6)), ItemGroup.BUILDING_BLOCKS);
+                    UniformIntProvider.create(2, 6)), ModItemGroups.MALECHIA_BLOCKS);
     public static final Block DEEPSLATE_ALOTIC_ORE = registerBlock("deepslate_alotic_ore",
             new OreBlock(FabricBlockSettings.of(Material.STONE).strength(30f).requiresTool(),
-                    UniformIntProvider.create(2, 6)), ItemGroup.BUILDING_BLOCKS);
+                    UniformIntProvider.create(2, 6)), ModItemGroups.MALECHIA_BLOCKS);
     public static final Block ERETH_ORE = registerBlock("ereth_ore",
             new OreBlock(FabricBlockSettings.of(Material.STONE).strength(20f).requiresTool(),
-                    UniformIntProvider.create(2, 6)), ItemGroup.BUILDING_BLOCKS);
+                    UniformIntProvider.create(2, 6)), ModItemGroups.MALECHIA_BLOCKS);
     public static final Block DEEPSLATE_ERETH_ORE = registerBlock("deepslate_ereth_ore",
             new OreBlock(FabricBlockSettings.of(Material.STONE).strength(30f).requiresTool(),
-                    UniformIntProvider.create(2, 6)), ItemGroup.BUILDING_BLOCKS);
+                    UniformIntProvider.create(2, 6)), ModItemGroups.MALECHIA_BLOCKS);
     public static final Block CHRYON_ORE = registerBlock("chryon_ore",
             new OreBlock(FabricBlockSettings.of(Material.STONE).strength(20f).requiresTool(),
-                    UniformIntProvider.create(2, 6)), ItemGroup.BUILDING_BLOCKS);
+                    UniformIntProvider.create(2, 6)), ModItemGroups.MALECHIA_BLOCKS);
     public static final Block DEEPSLATE_CHRYON_ORE = registerBlock("deepslate_chryon_ore",
             new OreBlock(FabricBlockSettings.of(Material.STONE).strength(30f).requiresTool(),
-                    UniformIntProvider.create(2, 6)), ItemGroup.BUILDING_BLOCKS);
+                    UniformIntProvider.create(2, 6)), ModItemGroups.MALECHIA_BLOCKS);
     public static final Block HALORIUM_ORE = registerBlock("halorium_ore",
             new OreBlock(FabricBlockSettings.of(Material.STONE).strength(20f).requiresTool(),
-                    UniformIntProvider.create(2, 6)), ItemGroup.BUILDING_BLOCKS);
+                    UniformIntProvider.create(2, 6)), ModItemGroups.MALECHIA_BLOCKS);
     public static final Block DEEPSLATE_HALORIUM_ORE = registerBlock("deepslate_halorium_ore",
             new OreBlock(FabricBlockSettings.of(Material.STONE).strength(30f).requiresTool(),
-                    UniformIntProvider.create(2, 6)), ItemGroup.BUILDING_BLOCKS);
+                    UniformIntProvider.create(2, 6)), ModItemGroups.MALECHIA_BLOCKS);
     public static final Block LITRIUM_ORE = registerBlock("litrium_ore",
             new OreBlock(FabricBlockSettings.of(Material.STONE).strength(20f).requiresTool(),
-                    UniformIntProvider.create(2, 6)), ItemGroup.BUILDING_BLOCKS);
+                    UniformIntProvider.create(2, 6)), ModItemGroups.MALECHIA_BLOCKS);
     public static final Block DEEPSLATE_LITRIUM_ORE = registerBlock("deepslate_litrium_ore",
             new OreBlock(FabricBlockSettings.of(Material.STONE).strength(30f).requiresTool(),
-                    UniformIntProvider.create(2, 6)), ItemGroup.BUILDING_BLOCKS);
+                    UniformIntProvider.create(2, 6)), ModItemGroups.MALECHIA_BLOCKS);
     public static final Block ALLIAN_ORE = registerBlock("allian_ore",
             new OreBlock(FabricBlockSettings.of(Material.STONE).strength(20f).requiresTool(),
-                    UniformIntProvider.create(6, 10)), ItemGroup.BUILDING_BLOCKS);
+                    UniformIntProvider.create(6, 10)), ModItemGroups.MALECHIA_BLOCKS);
     public static final Block DEEPSLATE_ALLIAN_ORE = registerBlock("deepslate_allian_ore",
             new OreBlock(FabricBlockSettings.of(Material.STONE).strength(30f).requiresTool(),
-                    UniformIntProvider.create(6, 10)), ItemGroup.BUILDING_BLOCKS);
+                    UniformIntProvider.create(6, 10)), ModItemGroups.MALECHIA_BLOCKS);
     public static final Block MALECHIA_BLOCK = registerBlock("malechia_block",
-            new Block(FabricBlockSettings.of(Material.METAL).strength(100f).requiresTool().luminance(15)), ItemGroup.BUILDING_BLOCKS);
+            new Block(FabricBlockSettings.of(Material.METAL).strength(100f).requiresTool().luminance(15)), ModItemGroups.MALECHIA_BLOCKS);
     public static final Block BOSS_ESSENCE_IRON_BLOCK = registerBlock("boss_essence_iron_block",
-            new Block(FabricBlockSettings.of(Material.METAL).strength(50f).requiresTool().luminance(4)), ItemGroup.BUILDING_BLOCKS);
+            new Block(FabricBlockSettings.of(Material.METAL).strength(50f).requiresTool().luminance(4)), ModItemGroups.MALECHIA_BLOCKS);
 
     private static Block registerBlock(String name, Block block, ItemGroup group) {
         registerBlockItem(name, block, group);
